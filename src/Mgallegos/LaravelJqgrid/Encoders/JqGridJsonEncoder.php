a8d158ab6229097e94e4c8e876b29c2691e76604
Mario Gallegos <mgallegos76@gmail.com>
2017-03-15 16:20:49 -0600

    Issue #38: return statement added when export method exist

--- src/Mgallegos/LaravelJqgrid/Encoders/JqGridJsonEncoder.php
+++ src/Mgallegos/LaravelJqgrid/Encoders/JqGridJsonEncoder.php
@@ -272,7 +272,7 @@ class JqGridJsonEncoder implements RequestedDataInterface {
 
 			if(method_exists($Repository, $method_name) )
 			{
-				$Repository->$method_name(
+				return $Repository->$method_name(
 					array_merge(
 						['rows'=> $rows],
 						['postedData'=> $postedData]
